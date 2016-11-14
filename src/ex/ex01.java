16405af1c650f3d1b94c0aba852632a971e0b354
kevin50406418 <kevin50406418@gmail.com>
2016-11-14 12:09:21 +0800

    init

---
+++ src/ex/ex01.java
@@ -0,0 +1,13 @@
+package ex;
+/*
+ * Date: 2016/11/14
+ * Author: 1050210XX OOO
+ */
+public class ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
